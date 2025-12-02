41eaddecfa9fa2e617ec9150ea523bf1ff6f55ca
Athamezia Avila <athameziaavila@gmail.com>
2025-12-02 13:38:07 +0700

    Commit

---
+++ src/main/java/com/mycompany/mahasiswa_50422288/Mahasiswa_50422288.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mahasiswa_50422288;
+
+/**
+ *
+ * @author Athamezia Avila
+ */
+public class Mahasiswa_50422288 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
